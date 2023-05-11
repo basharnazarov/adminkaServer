cc4b2c9470a9689d7b6f5a0eeb51997232394953
Bashar Nazarov Home Desktop <bashar.odilovich@gmail.com>
2023-05-12 00:19:13 +0500

    fixed cors policy

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors())
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   })
+// );
 
 const connection = mysql.createConnection({
   user: process.env.DB_USER,
